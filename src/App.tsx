1e6afed8e91e1fecc3e570368a6c1deefdd27f57
Ilya <illiai@wix.com>
2019-12-31 12:36:49 +0200

    Add initial commit with external css files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <h1 className="App-logo">Lean Watch</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
