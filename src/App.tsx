a111566334af39e309456a7bf23fd2009f01e685
Ilya <illiai@wix.com>
2019-12-31 13:08:10 +0200

    Add two static stacks

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,39 @@ import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <header>
-        <h1 className="App-logo">Lean Watch</h1>
-      </header>
+    <div className="board">
+      <div className="stack">
+        <h4>Title</h4>
+        <Card
+          imageUrl="https://www.fujifilm.eu/fileadmin/product_migration/dc/samples/ff_x20_008_12.JPG"
+          title="My Card"
+        />
+        <Card
+          imageUrl="https://www.fujifilm.eu/fileadmin/product_migration/dc/samples/ff_x20_008_12.JPG"
+          title="My Card"
+        />
+      </div>
+
+      <div className="stack">
+        <h4>Title</h4>
+        <Card
+          imageUrl="https://www.fujifilm.eu/fileadmin/product_migration/dc/samples/ff_x20_008_12.JPG"
+          title="My Card"
+        />
+        <Card
+          imageUrl="https://www.fujifilm.eu/fileadmin/product_migration/dc/samples/ff_x20_008_12.JPG"
+          title="My Card"
+        />
+      </div>
     </div>
   );
 };
+
+const Card = ({ title, imageUrl }: { title: string; imageUrl: string }) => (
+  <div className="card">
+    <img className="card-image" width={200} src={imageUrl} alt="some text" />
+    <span className="card-title">{title}</span>
+  </div>
+);
 
 export default App;
