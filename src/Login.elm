116dc7d931b8870f66913861ee8721251506acf3
Ilya <static.ila@gmail.com>
2020-01-15 17:38:16 +0200

    Extract full-fledged login flow. Ready to incorporate boards state

--- src/Login.elm
+++ src/Login.elm
@@ -1,20 +1,12 @@
-module Login exposing (..)
+port module Login exposing (..)
 
-import Html exposing (Attribute, Html, div, input)
-import Html.Attributes exposing (autofocus, placeholder, tabindex, type_, value)
+import ExtraEvents exposing (classIf, onClickIf)
+import Html exposing (Attribute, Html, a, button, div, h3, img, input, span, text)
+import Html.Attributes exposing (autofocus, class, classList, disabled, href, placeholder, src, tabindex, type_, value)
 import Html.Events exposing (onInput)
 
 
-type Msg
-    = SetEmail String
-    | SetPassword String
-
-
-init : Creds
-init =
-    { email = ""
-    , password = ""
-    }
+port login : Creds -> Cmd msg
 
 
 type alias Creds =
@@ -23,18 +15,103 @@ type alias Creds =
     }
 
 
-update : Msg -> Creds -> ( Creds, Cmd msg )
+type Msg
+    = SetEmail String
+    | SetPassword String
+    | OnLogin
+    | OnLoginSuccess LoginResponse
+    | OnLoginError LoginResponse
+
+
+port onLoginSuccess : (LoginResponse -> msg) -> Sub msg
+
+
+port onLoginError : (LoginResponse -> msg) -> Sub msg
+
+
+
+--MODEL
+
+
+init : Model
+init =
+    { email = ""
+    , password = ""
+    , loginStatus = Ready
+    }
+
+
+type alias Model =
+    { email : String
+    , password : String
+    , loginStatus : LoginStatus
+    }
+
+
+type LoginStatus
+    = Ready
+    | Loading
+    | LoginError
+
+
+type alias LoginResponse =
+    { foo : String
+    }
+
+
+
+--UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         SetPassword password ->
-            ( { email = model.email, password = password }, Cmd.none )
+            ( { model | password = password }, Cmd.none )
 
         SetEmail email ->
-            ( { email = email, password = model.password }, Cmd.none )
+            ( { model | email = email }, Cmd.none )
+
+        OnLogin ->
+            ( { model | loginStatus = Loading }, login { email = model.email, password = model.password } )
+
+        OnLoginSuccess res ->
+            ( { model | loginStatus = Ready }, Cmd.none )
+
+        OnLoginError res ->
+            ( { model | loginStatus = LoginError }, Cmd.none )
 
 
-viewEmailAndPassword model =
-    div []
-        [ input [ type_ "email", tabindex 1, autofocus True, placeholder "Enter email", value model.email, onInput SetEmail ] []
-        , input [ type_ "password", tabindex 2, placeholder "Enter password", value model.password, onInput SetPassword ] []
+
+--VIEW
+
+
+viewLogin : Model -> Html Msg
+viewLogin model =
+    let
+        isLoading =
+            model.loginStatus == Loading
+    in
+    div [ class "form-container" ]
+        [ div [ classList [ ( "form", True ), ( "error", model.loginStatus == LoginError ) ] ]
+            [ viewLoginStatus model.loginStatus
+            , h3 [] [ text "Log in to Lean Watch" ]
+            , input [ type_ "email", tabindex 1, autofocus True, placeholder "Enter email", value model.email, onInput SetEmail ] []
+            , input [ type_ "password", tabindex 2, placeholder "Enter password", value model.password, onInput SetPassword ] []
+            , button [ class "button flat", classIf isLoading "disabled", disabled isLoading, tabindex 3, onClickIf (not isLoading) OnLogin ] [ span [ classIf isLoading "spinner-border" ] [], text "Log In" ]
+            , div [ class "or-label" ] [ text "OR" ]
+            , button [ class "button material", tabindex 4 ] [ img [ class "google-icon", src "/icons/google.svg" ] [], text "Log in with Google" ]
+            , div [ class "line" ] []
+            , div [] [ text "Private testing is taking place. We are sorry that you can't sign up yet" ]
+            , div [] [ a [ href "mailto: static.ila@gmail.com" ] [ text "Ask us for an account thought ;)" ] ]
+            ]
         ]
+
+
+viewLoginStatus status =
+    case status of
+        LoginError ->
+            div [ class "login-status" ] [ text "Sorry, but we couldn't find user with such credentials :(" ]
+
+        _ ->
+            div [] []
--- src/Spa.elm
+++ src/Spa.elm
@@ -1,22 +1,11 @@
-port module Spa exposing (main)
+module Spa exposing (main)
 
 import Browser exposing (Document)
 import Browser.Navigation as Nav
-import Html exposing (Attribute, Html, a, button, div, h3, img, input, span, text)
-import Html.Attributes exposing (autofocus, class, classList, disabled, href, placeholder, src, tabindex, type_, value)
-import Html.Events exposing (onClick, onInput)
+import Html exposing (Attribute, Html, div, text)
 import Login as Login
 import Url exposing (Url)
 import Url.Parser as Parser exposing ((</>), Parser, s)
-
-
-port login : Login.Creds -> Cmd msg
-
-
-port onLoginSuccess : (LoginResponse -> msg) -> Sub msg
-
-
-port onLoginError : (LoginResponse -> msg) -> Sub msg
 
 
 main : Program () Model Msg
@@ -31,11 +20,6 @@ main =
         }
 
 
-type alias LoginResponse =
-    { foo : String
-    }
-
-
 
 -- MODEL
 
@@ -43,8 +27,7 @@ type alias LoginResponse =
 type alias Model =
     { page : Page
     , key : Nav.Key
-    , creds : Login.Creds
-    , loginStatus : LoginStatus
+    , login : Login.Model
     }
 
 
@@ -52,8 +35,7 @@ init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
     ( { page = urlToPage url
       , key = key
-      , creds = Login.init
-      , loginStatus = Ready
+      , login = Login.init
       }
     , Cmd.none
     )
@@ -63,21 +45,12 @@ type Msg
     = ClickedLink Browser.UrlRequest
     | ChangedUrl Url
     | LoginMsg Login.Msg
-    | OnLogin
-    | OnLoginSuccess LoginResponse
-    | OnLoginError LoginResponse
 
 
 type Page
     = Login
-    | Register
+    | Boards
     | NotFound
-
-
-type LoginStatus
-    = Ready
-    | Loading
-    | LoginError
 
 
 
@@ -88,21 +61,11 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         LoginMsg loginMsg ->
-            Login.update loginMsg model.creds
-                |> (\( newCred, cmd ) -> ( { model | creds = newCred }, cmd ))
+            Login.update loginMsg model.login
+                |> (\( login, cmd ) -> ( { model | login = login }, cmd ))
 
         ChangedUrl url ->
             ( { model | page = urlToPage url }, Cmd.none )
-
-        OnLogin ->
-            ( { model | loginStatus = Loading }, login model.creds )
-
-        --Assign login token, Redirect to the root
-        OnLoginSuccess res ->
-            ( { model | loginStatus = Ready }, Cmd.none )
-
-        OnLoginError res ->
-            ( { model | loginStatus = LoginError }, Cmd.none )
 
         ClickedLink urlRequest ->
             case urlRequest of
@@ -116,16 +79,17 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ onLoginSuccess OnLoginSuccess
-        , onLoginError OnLoginError
+        [ Login.onLoginSuccess Login.OnLoginSuccess
+        , Login.onLoginError Login.OnLoginError
         ]
+        |> Sub.map LoginMsg
 
 
 parser : Parser (Page -> a) a
 parser =
     Parser.oneOf
-        [ Parser.map Login Parser.top
-        , Parser.map Register (s "signup")
+        [ Parser.map Login (s "login")
+        , Parser.map Boards Parser.top
 
         --, Parser.map SelectedPhoto (s "photos" </> Parser.string)
         ]
@@ -143,7 +107,7 @@ urlToPage url =
 
 view : Model -> Document Msg
 view model =
-    { title = "Photo Groove, SPA Style"
+    { title = "Lean Watch"
     , body =
         [ div []
             [ viewPage model ]
@@ -154,80 +118,10 @@ view model =
 viewPage model =
     case model.page of
         Login ->
-            viewLogin model
+            Login.viewLogin model.login |> Html.map LoginMsg
 
-        Register ->
-            viewRegister model
+        Boards ->
+            text "Boards go to /login manually"
 
         NotFound ->
-            text "Not Found 404"
-
-
-viewLogin : Model -> Html Msg
-viewLogin model =
-    let
-        isLoading =
-            model.loginStatus == Loading
-    in
-    div [ class "form-container" ]
-        [ div [ classList [ ( "form", True ), ( "error", model.loginStatus == LoginError ) ] ]
-            [ viewLoginStatus model.loginStatus
-            , h3 [] [ text "Log in to Lean Watch" ]
-            , Login.viewEmailAndPassword model.creds |> Html.map LoginMsg
-            , button [ class "button flat", classIf isLoading "disabled", disabled isLoading, tabindex 3, onClickIf (not isLoading) OnLogin ] [ span [ classIf isLoading "spinner-border" ] [], text "Log In" ]
-            , div [ class "or-label" ] [ text "OR" ]
-            , button [ class "button material", tabindex 4 ] [ img [ class "google-icon", src "/icons/google.svg" ] [], text "Log in with Google" ]
-            , div [ class "line" ] []
-            , div [] [ text "Private testing is taking place. We are sorry that you can't sign up yet" ]
-            , div [] [ a [ href "mailto: static.ila@gmail.com" ] [ text "Ask us for an account thought ;)" ] ]
-            ]
-        ]
-
-
-viewLoginStatus status =
-    case status of
-        LoginError ->
-            div [ class "login-status" ] [ text "Sorry, but we couldn't find user with such credentials :(" ]
-
-        _ ->
-            div [] []
-
-
-viewRegister model =
-    div [ class "form-container" ]
-        [ div [ class "form" ]
-            [ h3 [] [ text "Sign up to Lean Watch" ]
-            , input [ type_ "email", placeholder "Enter email" ] []
-            , div [ class "button flat disabled" ] [ text "Continue" ]
-            , div [ class "or-label" ] [ text "OR" ]
-            , div [ class "button material" ] [ img [ class "google-icon", src "/icons/google.svg" ] [], text "Continue with Google" ]
-            , div [ class "line" ] []
-            , div [] [ a [ href "/" ] [ text "Already have an account? Log in" ] ]
-            ]
-        ]
-
-
-onClickIf : Bool -> Msg -> Attribute Msg
-onClickIf condition msg =
-    if condition then
-        onClick msg
-
-    else
-        emptyAttribute
-
-
-classIf : Bool -> String -> Attribute Msg
-classIf condition className =
-    if condition then
-        class className
-
-    else
-        emptyAttribute
-
-
-emptyAttribute =
-    class ""
-
-
-
---Login Page
+            text "NotFound"
